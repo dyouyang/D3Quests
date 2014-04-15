94bfac7d6a0f8b74da664714be84df4d25238f37
dyouyang <douyang@gmail.com>
2014-04-15 00:30:31 -0700

    Comment and refactor HeroesActivity

--- src/com/dyo/d3quests/HeroesActivity.java
+++ src/com/dyo/d3quests/HeroesActivity.java
@@ -8,18 +8,13 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Set;
-import java.util.zip.Inflater;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import com.flurry.android.FlurryAgent;
 
 import android.app.ActionBar;
 import android.app.ActionBar.OnNavigationListener;
@@ -53,59 +48,72 @@ import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.flurry.android.FlurryAgent;
+
+/**
+ * @author yinglong
+ * 
+ * Main activity of the app.  Allows user to input a battle tag to retrieve a list of heroes on the account.
+ * Selecting a hero will open up the details activity.  Also has a side navigation menu (which is used to store
+ * favorited heroes)
+ */
 public class HeroesActivity extends Activity implements OnNavigationListener{
 
-
+	// The UI consists of battle tag input, a button to get heroes,
+	// and the list of heroes for an account.
 	private AutoCompleteTextView battleTagInput;
 	private EditText battleTagNumInput;
-	private Button findQuests;
+	private Button getHeroes;
 	private ListView heroesView;
 	
+	// Variables related to an account.
 	private String battleTag;
 	private String battleTagNum;
-	HashMap<String, Integer> heroesMap = new HashMap<String, Integer>();
-	ArrayList<Hero> heroesList = new ArrayList<Hero>();
-	ArrayAdapter<Hero> adapter;
-	
-	ActionBar actionBar;
-	SpinnerAdapter mSpinnerAdapter;
 	private String region = "us";
+
+	ArrayList<Hero> heroesList = new ArrayList<Hero>();
+	ArrayAdapter<Hero> heroesListAdapter;
+
+	// Spinner for account region.
+	SpinnerAdapter mSpinnerAdapter;
 	private String [] regions = {"us", "eu", "kr", "tw"};
-	
+
+	// Autocomplete previously entered battle tags.
 	SharedPreferences settings;
 	Set<String> recentAccounts;
 	ArrayAdapter<String> adapterAutoComplete;
 	private ArrayList<String> recentAccountsList;
-	
+
+	// Left navigation drawer.
 	private DrawerLayout mDrawerLayout;
 	private ActionBarDrawerToggle mDrawerToggle;
 	private ListView mDrawerList;
 	private ArrayAdapter<SavedHero> drawerAdapter;
-	
+
+	// Datasource to access saved heroes DB.
 	private HeroesDataSource datasource;
 	
-	
+	ActionBar actionBar;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_heroes);
+
+		// Set up action bar with spinner.
 		setTitle("D3 Helper");
-		
-		//recentAccounts = new LinkedHashMap<String, String>(5, (float) 0.75, true);
 		actionBar = getActionBar();
+		actionBar.setNavigationMode(getActionBar().NAVIGATION_MODE_LIST);
 		mSpinnerAdapter = ArrayAdapter.createFromResource(actionBar.getThemedContext(), R.array.action_list,
 		          android.R.layout.simple_spinner_dropdown_item);
-		
-		actionBar.setNavigationMode(getActionBar().NAVIGATION_MODE_LIST);
-		
 		actionBar.setListNavigationCallbacks(mSpinnerAdapter, this);
-		//actionBar.setDisplayShowTitleEnabled(false);
-		
+
+		// Get saved heroes for navigation drawer.
 		datasource = new HeroesDataSource(this);
 		datasource.open();
-		
 		List<SavedHero> savedHeroes = datasource.getAllHeroes();
 		
+		// Set up navigation drawer for saved heroes.
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, 
@@ -125,7 +133,8 @@ public class HeroesActivity extends Activity implements OnNavigationListener{
                 invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
             }
         };
-        // Set the drawer toggle as the DrawerListener
+
+        // Set the drawer toggle as the DrawerListener.
         mDrawerLayout.setDrawerListener(mDrawerToggle);
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
@@ -141,68 +150,71 @@ public class HeroesActivity extends Activity implements OnNavigationListener{
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
         mDrawerList.setOnItemLongClickListener(new DrawerItemLongClickListener());
         
-        
+        // Retrieve saved accounts from sharedprefs for battle tag autocomplete.
 		settings = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-
 		recentAccounts = settings.getStringSet("recentAccounts", new HashSet<String>());
 		recentAccountsList = new ArrayList<String>(recentAccounts);
 		adapterAutoComplete = new ArrayAdapter<String>(
 				this, android.R.layout.simple_dropdown_item_1line, recentAccountsList);
-			
+
+		// Input is the left input for account string, NumInput for code portion.
 		battleTagInput = (AutoCompleteTextView) findViewById(R.id.battletag);
-
 		battleTagInput.setAdapter(adapterAutoComplete);
-
 		battleTagNumInput = (EditText) findViewById(R.id.battletag_num);
 		
-		// Onclick for autocomplete suggestion.
+		// Onclick for an autocomplete suggestion.
 		battleTagInput.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> parent, View v, int position,
 					long id) {
+
+					// If a suggestion is clicked, split the battle tag on #,
+					// then insert the left and right parts.
 					String item = ((TextView)v).getText().toString();
 					String [] battleTagSplit = item.split("#");
 					if (battleTagSplit.length == 2) {
 						battleTagInput.setText(battleTagSplit[0]);
 						battleTagNumInput.setText(battleTagSplit[1]);
 					} else {
+						// If somehow we don't get a string/num split, report error.
 						Toast.makeText(getApplicationContext(), "Battle Tag error", Toast.LENGTH_SHORT).show();
 					}
 			}
-			
 		});
 
-		// Fill in id from last button click.
-		// Note, fields must be set explicity first for auto-loading list on resume feature.
+		// Fill in id from last button click from shared prefs.
+		// Note, fields must be set explicity once first for auto-loading list on resume feature.
 		battleTag = settings.getString("battleTag", "");
 		battleTagInput.setText(battleTag); 
 		battleTagNum = settings.getString("battleTagNum", "");
 		battleTagNumInput.setText(battleTagNum);
+		// Default region to 0, or US
 		region = regions[settings.getInt("region", 0)];
 		actionBar.setSelectedNavigationItem(settings.getInt("region", 0));
-		
-		findQuests = (Button) findViewById(R.id.findQuests);
-		findQuests.setOnClickListener(new OnClickListener() {
-			
+
+		// Button to retrieve heroes from account.
+		getHeroes = (Button) findViewById(R.id.get_heroes_button);
+		getHeroes.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				InputMethodManager inputManager = (InputMethodManager)
                         getSystemService(Context.INPUT_METHOD_SERVICE); 
-
-inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+				inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
                            InputMethodManager.HIDE_NOT_ALWAYS);
+
 				// Remove focus from the autocompletetextview to prevent
 				// flashing of the dropdown.
 				getCurrentFocus().clearFocus();
 				battleTag = battleTagInput.getText().toString();
-				battleTag = battleTag.replaceAll("\\s","");
-				battleTagInput.setText(battleTag);
+				battleTag = battleTag.replaceAll("\\s",""); 	// Strip whitespace.
+				battleTagInput.setText(battleTag);				// Replace stripped version back in.
 				battleTagNum = battleTagNumInput.getText().toString();
-				
+
 				if (battleTag.length() < 1 || battleTagNum.length() < 1) {
 					Toast.makeText(getApplicationContext(), "Enter Battle.net ID and 4 digit code", Toast.LENGTH_LONG).show();
-					adapter.clear();
+					heroesListAdapter.clear();
 					return;
 				}
 				
@@ -224,15 +236,13 @@ inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
 		        } else {
 		            Toast.makeText(getApplicationContext(), "No network connection.", Toast.LENGTH_LONG).show();
 		        }
-		        
-				//missingQuests.setText(battleTag);				
 			}
 		});
 		
 		heroesView = (ListView) findViewById(R.id.missingQuests);
-    	adapter = new ArrayAdapter<Hero>(this, 
+    	heroesListAdapter = new ArrayAdapter<Hero>(this, 
     	        android.R.layout.simple_list_item_1, heroesList);	
-    	heroesView.setAdapter(adapter);	
+    	heroesView.setAdapter(heroesListAdapter);	
     	
     	heroesView.setOnItemClickListener(new OnItemClickListener() {
 
@@ -242,7 +252,7 @@ inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
 				String item = ((TextView)view).getText().toString();
 				
 				Intent i = new Intent(view.getContext(), QuestsActivitySwipe.class);
-				Hero hero = (Hero)adapter.getItem(position);
+				Hero hero = (Hero)heroesListAdapter.getItem(position);
 				i.putExtra("heroId", hero.id);
 				i.putExtra("heroName", hero.name);
 				i.putExtra("battleTagFull", battleTag + "-" + battleTagNum);
@@ -256,7 +266,7 @@ inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
 			@Override
 			public boolean onItemLongClick(AdapterView<?> arg0, View v,
 					int position, long id) {
-				Hero hero = (Hero)adapter.getItem(position);
+				Hero hero = (Hero)heroesListAdapter.getItem(position);
 				addSavedHero(hero.id, hero.name, hero.level, hero.d3class, battleTag + "-" + battleTagNum, region);
 				return true;
 			}
@@ -265,10 +275,10 @@ inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
     	// Auto load heroes on first app open
     	if (battleTagInput.getText().toString().length() > 0
     			&& battleTagNumInput.getText().toString().length() > 0) {
-    		findQuests.post(new Runnable() {
+    		getHeroes.post(new Runnable() {
 				@Override
 				public void run() {
-					findQuests.performClick();
+					getHeroes.performClick();
 					
 				}
 			});
@@ -352,9 +362,9 @@ inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
         // onPostExecute displays the results of the AsyncTask.
         @Override
         protected void onPostExecute(String result) {
-        	adapter.clear();
+        	heroesListAdapter.clear();
         	parseHeroes(result);
-        	adapter.notifyDataSetChanged();
+        	heroesListAdapter.notifyDataSetChanged();
         	mProgress.hide();
         	// TODO: Handle updates better (without notifying).
             //missingQuests.setText(heroesList.toString());
